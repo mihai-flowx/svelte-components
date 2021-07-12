bd46e0552a42d68c73cbb3fe96f9893cd4bd22c8
Mihai Saru <mihai@flowx.ai>
2021-07-12 23:54:33 +0300

    init: project & postcss+tailwind in component style

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let name
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <button class="btn-indigo">Click me</button>
+</main>
+
+<style lang="postcss">
+  .btn-indigo {
+    @apply py-2 px-4 bg-indigo-500 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-75;
+  }
+</style>
